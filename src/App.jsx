1de49f917dc7852fbf277547a03785f4fde26c21
luisjairm <luisjairm.dev@gmail.com>
2023-03-31 13:39:24 -0600

    105. Creando una Animación en el Modal

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ function App () {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
 
   const handleNuevoGasto = () => {
     setModal(true)
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
   }
 
   return (
@@ -33,6 +37,8 @@ function App () {
         modal && (
           <Modal
             setModal={setModal}
+            animarModal={animarModal}
+            setAnimarModal={setAnimarModal}
           />
         )
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,11 @@
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({ setModal }) => {
+const Modal = ({ setModal, animarModal, setAnimarModal }) => {
   const ocultarModal = () => {
-    setModal(false)
+    setAnimarModal(false)
+    setTimeout(() => {
+      setModal(false)
+    }, 500)
   }
   return (
     <div className='modal'>
@@ -12,6 +15,9 @@ const Modal = ({ setModal }) => {
           onClick={ocultarModal}
         />
       </div>
+      <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+        <legend>Nuevo Gasto</legend>
+      </form>
     </div>
   )
 }
