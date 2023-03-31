bffc2b0334c0bb707920999df97005117641e392
luisjairm <luisjairm.dev@gmail.com>
2023-03-31 13:52:15 -0600

    106. Añadiendo el Formulario de Gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,6 +17,40 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
       </div>
       <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
         <legend>Nuevo Gasto</legend>
+        <div className='campo'>
+          <label htmlFor='nombre'>Nombre Gasto</label>
+          <input
+            id='nombre'
+            type='text'
+            placeholder='Añade el nombre del gasto'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='nombre'>Cantidad</label>
+          <input
+            id='cantidad'
+            type='number'
+            placeholder='Añade la cantidad del gastos ej: 300'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='categoria'>Categoría</label>
+          <select id='categoria'>
+            <option value='seleccione'>-- Seleccione --</option>
+            <option value='ahorro'>Ahorro</option>
+            <option value='comida'>Comida</option>
+            <option value='casa'>Casa</option>
+            <option value='gastos'>Gastos Varios</option>
+            <option value='ocio'>Ocio</option>
+            <option value='salud'>Salud</option>
+            <option value='suscripciones'>Suscripciones</option>
+          </select>
+        </div>
+
+        <input
+          type='submit'
+          value='Añadir Gastos'
+        />
       </form>
     </div>
   )
