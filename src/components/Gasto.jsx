8e8d08672114de0cadab5cc1910d60514a306388
luisjairm <luisjairm.dev@gmail.com>
2023-04-02 21:41:51 -0600

    111. Mostrando los Gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Gasto = ({ gasto }) => {
   return (
-    <div>Gasto</div>
+    <div
+      className='gasto sombra'
+    >
+      <div
+        className='contenido-gasto'
+      >
+        <div className='descripcion-gasto'>
+          <p className='categoria'>{gasto.categoria}</p>
+          <p className='nombre-gasto'>{gasto.nombre}</p>
+          <p className='gasto-cantidad'>{gasto.cantidad}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
