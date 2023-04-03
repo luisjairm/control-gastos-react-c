6d73d2cbe4836ac974a0d25bfca8ac714d74b2bf
luisjairm <luisjairm.dev@gmail.com>
2023-04-02 21:31:06 -0600

    110. Creando el Componente para Crear un Gasto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Modal from './components/Modal'
 import IconNuevoGasto from './img/nuevo-gasto.svg'
 import { generarId } from './helpers/index'
+import ListadoGastos from './components/ListadoGastos'
 
 function App () {
   const [presupuesto, setPresupuesto] = useState(0)
@@ -37,13 +38,20 @@ function App () {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto && (
-        <div className='nuevo-gasto'>
-          <img
-            src={IconNuevoGasto}
-            alt='nuevo gasto'
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main>
+          <div className='nuevo-gasto'>
+            <img
+              src={IconNuevoGasto}
+              alt='nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
       {
         modal && (
---
+++ src/components/Gasto.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gasto = ({ gasto }) => {
+  return (
+    <div>Gasto</div>
+  )
+}
+
+export default Gasto
