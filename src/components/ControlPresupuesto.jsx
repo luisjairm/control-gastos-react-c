e5922dd88b2944188cbe1cd963a572f44a9eaeef
luisjairm <luisjairm.dev@gmail.com>
2023-04-04 14:53:23 -0600

    132. Cambiando la apariencia de Presupuesto Disponible si ya fue agotado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -33,9 +33,9 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
       <div>
         <CircularProgressbar
           styles={buildStyles({
-            pathColor: '#3B82F6',
+            pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
             trailColor: '#F5F5F5',
-            textColor: '#3B82F6'
+            textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6'
           })}
           value={porcentaje}
           text={`${porcentaje}% Gastado`}
@@ -43,7 +43,7 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
       </div>
       <div className='contenido-presupuesto'>
         <p><span>Presupuesto: </span>{formatearCantidad(presupuesto)}</p>
-        <p><span>Disponible: </span>{formatearCantidad(disponible)}</p>
+        <p className={`${disponible < 0 ? 'negativo' : ''}`}><span>Disponible: </span>{formatearCantidad(disponible)}</p>
         <p><span>Gastado: </span>{formatearCantidad(gastado)}</p>
       </div>
     </div>
